9286e09b19165d5687dc9da3bb750fbb499bc7b9
GhostNinja <121746490+DghostNinja@users.noreply.github.com>
2025-03-10 00:04:39 +0100

    Update background.js

--- background.js
+++ background.js
@@ -1,8 +1,12 @@
-chrome.webRequest.onBeforeRequest.addListener(
-  function(details) {
+const isFirefox = typeof browser !== "undefined"; // Detect Firefox
+
+const webRequest = isFirefox ? browser.webRequest : chrome.webRequest;
+
+webRequest.onBeforeRequest.addListener(
+  function (details) {
     let url = new URL(details.url);
     let redirectParams = ["redirect", "url", "next", "return", "returnTo", "destination", "goto"];
-    
+
     for (let param of redirectParams) {
       if (url.searchParams.has(param)) {
         console.warn(`[!!] Possible Open Redirect Detected: ${details.url}`);
@@ -10,5 +14,5 @@ chrome.webRequest.onBeforeRequest.addListener(
     }
   },
   { urls: ["<all_urls>"] },
-  ["blocking"]
+  isFirefox ? [] : ["blocking"] // Firefox doesn't support "blocking"
 );
